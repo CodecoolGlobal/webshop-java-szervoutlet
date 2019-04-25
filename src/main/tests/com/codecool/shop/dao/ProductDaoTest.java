9d020b52584af029f1bd288e6a4e11b357f60c4a
gaborTachtler <tachtler@gmail.com>
2019-04-25 17:59:03 +0200

    fix the ProductDao test issue

--- src/main/tests/com/codecool/shop/dao/ProductDaoTest.java
+++ src/main/tests/com/codecool/shop/dao/ProductDaoTest.java
@@ -4,7 +4,9 @@ import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ class ProductDaoTest {
     private ProductDao productDao = ProductDaoMem.getInstance();
     private Supplier testSupplier = new Supplier(1, "test", "description");
     private ProductCategory testProductCat = new ProductCategory(1, "name", "department", "description");
-    private Product testProduct = new Product(1, "test", 1, "USD", "description", testProductCat, testSupplier);
+    private Product testProduct = new Product(1, "test", 1, "USD", "description", testProductCat, testSupplier, 1);
 
     @BeforeEach
     void init() {
@@ -41,7 +43,7 @@ class ProductDaoTest {
 
     @Test
     void testIsProductAdded() {
-        productDao.add(new Product(2, "test", 2, "USD", "description", new ProductCategory(2, "name", "department", "description"), new Supplier(2, "test", "description")));
+        productDao.add(new Product(2, "test", 2, "USD", "description", new ProductCategory(2, "name", "department", "description"), new Supplier(2, "test", "description"),1));
         List<Product> products = productDao.getAll();
         assertEquals(2, products.get(products.size() - 1).getId());
         productDao.remove(products.get(products.size() - 1).getId());
@@ -55,7 +57,7 @@ class ProductDaoTest {
 
     @Test
     void testIsProductRemoved() {
-        Product p = new Product(3, "test", 4, "USD", "description", new ProductCategory(3, "name", "department", "description"), new Supplier(3, "test", "description"));
+        Product p = new Product(3, "test", 4, "USD", "description", new ProductCategory(3, "name", "department", "description"), new Supplier(3, "test", "description"),1);
         productDao.add(p);
         int id = p.getId();
         productDao.remove(id);
